9ca40abd8a7c9c1f7f603051a87c1ee9c689bcb9
bilalghourani <bilalghourani92@gmail.com>
2024-09-23 06:26:20 +0300

    build 1.0.1(9)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,7 +23,7 @@ android {
         applicationId = "com.grid.gridlicense"
         minSdk = 24
         targetSdk = 34
-        versionCode = 8
+        versionCode = 9
         versionName = "1.0.1"
         archivesName = "grid_license_release(${versionName}(${versionCode}))"
 
